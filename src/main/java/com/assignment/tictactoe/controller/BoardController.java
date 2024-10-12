19c904a7981130611ac8d08a115bd8dc9112b520
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-13 00:48:32 +0530

    best practices

--- src/main/java/com/assignment/tictactoe/controller/BoardController.java
+++ src/main/java/com/assignment/tictactoe/controller/BoardController.java
@@ -1,10 +1,6 @@
 package com.assignment.tictactoe.controller;
 
-import com.assignment.tictactoe.service.AIPlayer;
-import com.assignment.tictactoe.service.Board;
-import com.assignment.tictactoe.service.BoardImpl;
-import com.assignment.tictactoe.service.HumanPlayer;
-import com.assignment.tictactoe.service.Piece;
+import com.assignment.tictactoe.service.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -26,6 +22,9 @@ public class BoardController {
     private Label statusMessage;
     private Button[][] gameButtons;
     private Board board;
+    private HumanPlayer humanPlayer;
+    private AIPlayer aiPlayer;
+    private Piece currentPlayerPiece;
 
     @FXML
     public void initialize() {
@@ -34,10 +33,11 @@ public class BoardController {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 gameButtons[i][j] = (Button) gameBoard.getChildren().get(i * 3 + j);
-                gameButtons[i][j].setDisable(true);
+                gameButtons[i][j].setDisable(true);  // Disable until game starts
             }
         }
 
+        // Restrict player name input to alphabets
         playerNameField.addEventFilter(KeyEvent.KEY_TYPED, event -> {
             String character = event.getCharacter();
             if (!character.matches("[a-zA-Z]")) {
@@ -49,32 +49,56 @@ public class BoardController {
     @FXML
     public void startGame() {
         String playerName = playerNameField.getText();
-        Piece selectedPiece = pieceSelection.getValue();
+        currentPlayerPiece = pieceSelection.getValue();
 
-        if (selectedPiece == null) {
+        if (currentPlayerPiece == null) {
             statusMessage.setText("Please select a piece.");
             return;
         } else if (playerName == null || playerName.trim().isEmpty()) {
             statusMessage.setText("Please enter your name.");
             return;
         }
+        enableAllButtons();
 
+        board = new BoardImpl();
+        humanPlayer = new HumanPlayer(board, playerName, currentPlayerPiece);
+        aiPlayer = new AIPlayer(board, currentPlayerPiece == Piece.X ? Piece.O : Piece.X);
+        System.out.println(playerName+" s Piece is : "+humanPlayer.getSelectedPiece());
+        System.out.println("AI s Piece is :" +aiPlayer.getSelectedPiece());
+        statusMessage.setText(playerName + " vs AI - Game Started!");
+        startGameButton.setDisable(true);
+        playerNameField.setDisable(true);
+        pieceSelection.setDisable(true);
+        board.printBoard();
+    }
+
+    @FXML
+    public void handleButtonClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        int row = GridPane.getRowIndex(clickedButton);
+        int col = GridPane.getColumnIndex(clickedButton);
+
+        if (board.isLegalMove(row, col)) {
+            board.updateMove(row, col, humanPlayer.getSelectedPiece());
+            clickedButton.setText(humanPlayer.getSelectedPiece().toString());
+            clickedButton.setDisable(true);
+        } else {
+            statusMessage.setText("This move is not allowed. Try another spot.");
+        }
+    }
+
+    private void disableAllButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                gameButtons[i][j].setDisable(true);
+            }
+        }
+    }
+    private void enableAllButtons() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 gameButtons[i][j].setDisable(false);
             }
         }
-
-        board = new BoardImpl();
-        HumanPlayer humanPlayer = new HumanPlayer(board, playerName, selectedPiece);
-        AIPlayer aiPlayer = new AIPlayer(board, Piece.O);
-        statusMessage.setText(playerName + " vs AI - Game Started!");
-        startGameButton.setDisable(true);
-
-    }
-
-    @FXML
-    public void handleButtonClick(ActionEvent event) {
-
     }
 }
