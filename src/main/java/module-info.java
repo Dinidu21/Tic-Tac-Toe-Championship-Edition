8f078f563f4fd5ddda899b88d2c4e4244aa7ab4b
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-12 13:01:48 +0530

    add dependency lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.assignment.tictactoe.tictactoechampionshipedition {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens com.assignment.tictactoe to javafx.fxml;
     exports com.assignment.tictactoe;
+    opens com.assignment.tictactoe.controller to javafx.fxml;
 }
