7b60dae5e721c88f17fdad63073a02ca2e045e1b
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-12 14:49:03 +0530

    game initial state done

--- src/main/java/com/assignment/tictactoe/service/BoardImpl.java
+++ src/main/java/com/assignment/tictactoe/service/BoardImpl.java
@@ -18,34 +18,26 @@ public class BoardImpl implements Board, Serializable {
 
     @Override
     public void initializeBoard() {
-        for (int i = 0; i < pieces.length; i++) {
-            Arrays.fill(pieces[i], Piece.EMPTY);
+        for (Piece[] piece : pieces) {
+            Arrays.fill(piece, Piece.EMPTY);
         }
     }
 
     @Override
     public void printBoard() {
-        for (Piece[] row : pieces) {
-            System.out.println(Arrays.toString(row));
-        }
+
     }
 
     @Override
     public boolean isLegalMove(int row, int col) {
-        return (row >= 0 && row < 3 && col >= 0 && col < 3) && (pieces[row][col] == Piece.EMPTY);
+
+        return false;
     }
 
     @Override
     public void updateMove(int row, int col, Piece piece) {
-        if (!isLegalMove(row, col)) {
-            System.out.println("Please Enter a Legal Move");
-        } else {
-            pieces[row][col] = piece;
-            System.out.println("Move updated: " + piece + " placed at (" + row + ", " + col + ")");
-            if (boardUI != null) {
-                boardUI.update(col, row, true);
-            }
-        }
+
+
     }
 
     @Override
