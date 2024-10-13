dc7a0f7bd171fcdf2d7441691da373e37736a332
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-13 15:44:54 +0530

    Board Done

--- src/main/java/com/assignment/tictactoe/service/Board.java
+++ src/main/java/com/assignment/tictactoe/service/Board.java
@@ -3,10 +3,8 @@ package com.assignment.tictactoe.service;
 public interface Board {
     BoardUI getBoardUI();
     void initializeBoard();
-    boolean isLegalMove(int row,int col);
-    void updateMove(int row,int col,Piece piece);
+    boolean isLegalMove(int row, int col);
+    void updateMove(int row, int col, Piece piece);
     Winner checkWinner();
     void printBoard();
-    void update(int col, int row, boolean isHuman);
-    void notifyWinner();
 }
