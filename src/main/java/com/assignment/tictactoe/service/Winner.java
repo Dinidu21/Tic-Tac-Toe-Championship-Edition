556a040a8aae8f1d3de5dd23e5fb573633c0f662
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-13 15:46:51 +0530

    Winner Done

--- src/main/java/com/assignment/tictactoe/service/Winner.java
+++ src/main/java/com/assignment/tictactoe/service/Winner.java
@@ -13,13 +13,6 @@ import java.io.Serializable;
 
 public class Winner implements Serializable {
     private Piece winningPiece;
-    private int col1;
-    private int row1;
-    private int col2;
-    private int row2;
-    private int col3;
-    private int row3;
-
-    public Winner(Piece winningPiece) {this.winningPiece = winningPiece;
-    }
+    private int col1, row1, col2, row2, col3, row3;
+    public Winner(Piece winningPiece) {this.winningPiece = winningPiece;}
 }
