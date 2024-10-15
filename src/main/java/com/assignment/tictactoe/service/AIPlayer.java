4d15caae4d4a0da9a2495c45a3be6809b6cb3467
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-15 17:55:25 +0530

    make Easy Move Worked

--- src/main/java/com/assignment/tictactoe/service/AIPlayer.java
+++ src/main/java/com/assignment/tictactoe/service/AIPlayer.java
@@ -1,5 +1,6 @@
 package com.assignment.tictactoe.service;
 
+import com.assignment.tictactoe.controller.BoardController;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,19 +11,47 @@ import java.io.Serializable;
 @Setter
 public class AIPlayer extends Player implements Serializable {
     private Random random;
-    private Piece selectedPiece;
+    private Piece selectedPiece; // AI's piece (e.g., X or O)
+    private String difficulty;
 
-    public AIPlayer(Board board, Piece selectedPiece) {
+    public AIPlayer(Board board, Piece selectedPiece, String difficulty) {
         super(board);
+        if (board == null) {
+            throw new NullPointerException("Board cannot be null");
+        }
+        if (selectedPiece == null) {
+            throw new NullPointerException("Selected piece cannot be null");
+        }
         this.random = new Random();
         this.selectedPiece = selectedPiece;
+        this.difficulty = difficulty;
     }
 
     @Override
     public void move(int row, int col) {
-        int[] spot = ((BoardImpl) board).findNextAvailableSpot();
-        if (spot != null) {
+        if (difficulty.equals(BoardController.EASY)) {
+            System.out.println("Difficulty is Easy...");
+            makeEasyMove();
+        } else if (difficulty.equals(BoardController.HARD)) {
+            System.out.println("Difficulty is Hard...");
+            //makeHardMove();
+        }
+    }
+
+
+
+    private void makeEasyMove() {
+        try {
+            int[] spot = ((BoardImpl) board).findNextAvailableSpot();
+            if (spot == null) {
+                System.out.println("No available spot found.");
+                return;
+            }
             board.updateMove(spot[0], spot[1], selectedPiece);
+        } catch (ClassCastException e) {
+            System.err.println("Error: Board is not an instance of BoardImpl");
+        } catch (Exception e) {
+            System.err.println("Error updating board: " + e.getMessage());
         }
     }
 }
