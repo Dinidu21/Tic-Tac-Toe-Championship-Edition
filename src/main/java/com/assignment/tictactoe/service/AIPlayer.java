2513f112789f69ff006e69dc3c2f82faab4f5ef8
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-10-16 14:47:45 +0530

    UI Bug

--- src/main/java/com/assignment/tictactoe/service/AIPlayer.java
+++ src/main/java/com/assignment/tictactoe/service/AIPlayer.java
@@ -28,17 +28,51 @@ public class AIPlayer extends Player implements Serializable {
         }
     }
 
-//    public int[] findBestMove(Board board) {
-//        System.out.println("Find the best move method in AI Player");
-//
-//    }
+    public int[] findBestMove(Board board) {
+        int bestValue = Integer.MIN_VALUE;
+        int[] bestMove = new int[2]; // To store the best move as {row, col}
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // Check if the move is legal
+                if (board.isLegalMove(i, j)) {
+                    // Make the move
+                    board.updateMove(i, j, selectedPiece);
+                    // Call minimax
+                    int moveValue = minimax((BoardImpl) board, 0, false);
+                    // Undo the move
+                    board.updateMove(i, j, Piece.EMPTY);
+
+                    // If the value of this move is greater than the best value found
+                    if (moveValue > bestValue) {
+                        bestMove[0] = i; // Update best row
+                        bestMove[1] = j; // Update best column
+                        bestValue = moveValue; // Update best value
+                    }
+                }
+            }
+        }
+
+        // Return the best move found
+        return bestMove;
+    }
+
 
     private int minimax(BoardImpl board, int depth, boolean isMaximizing) {
-        Winner winner = board.checkWinner();
-        if (winner.getWinningPiece() == selectedPiece) return 10 - depth;
-        if (winner.getWinningPiece() == opponentPiece) return depth - 10;
-        if (isBoardFull()) return 0;
+        Winner winner = board.checkWinner(); // Check for a winner
 
+        // Check if the game is over and return the appropriate score
+        if (winner != null) {
+            if (winner.getWinningPiece() == selectedPiece) {
+                return 10 - depth; // AI wins
+            } else if (winner.getWinningPiece() == opponentPiece) {
+                return depth - 10; // Opponent wins
+            } else {
+                return 0; // Tie
+            }
+        }
+
+        // If the game is not over and we are maximizing
         if (isMaximizing) {
             int best = Integer.MIN_VALUE;
             for (int i = 0; i < 3; i++) {
@@ -52,6 +86,7 @@ public class AIPlayer extends Player implements Serializable {
             }
             return best;
         } else {
+            // If the game is not over and we are minimizing
             int best = Integer.MAX_VALUE;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -65,6 +100,7 @@ public class AIPlayer extends Player implements Serializable {
             return best;
         }
     }
+
 
     public boolean isBoardFull() {
         for (int i = 0; i < 3; i++) {
